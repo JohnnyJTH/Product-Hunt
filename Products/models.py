21937ba30d718313b71b6c0e5d51be0c3b8901a9
Johnny JTH <johanohly@gmail.com>
2021-01-17 15:35:44 +0100

    Added home and create posts

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,22 @@
+import Products
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=255)
+    body = models.TextField()
+
+    url = models.TextField()
+    votes = models.IntegerField(default=1)
+    pub_date = models.DateTimeField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def date(self):
+        return self.pub_date.strftime('%B %e, %Y')
